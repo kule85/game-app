626e5d58cf9e1374bd6a1c6e93dee9fb9536a52b
kule85 <kulence@gmail.com>
2023-04-30 20:02:25 +0200

    test

--- src/components/atoms/Card/index.tsx
+++ src/components/atoms/Card/index.tsx
@@ -15,7 +15,7 @@ type Props = {
 
 const Card: FC<Props> = ({ card, player }) => {
   const { isHumanPlay, onPlay } = useGame()
-  console.log('test dddsad')
+  console.log('test')
   return (
     <div
       className={`card ${player.isHumanPlayer ? 'human-card' : ''}`}
